e9e2e21c6efdbf41564d84c8b4c073e6f734ddb4
HarshadSanjayKarale <harshadkarale0@gmail.com>
2025-04-01 18:52:09 +0530

    trying to deploy

--- app.py
+++ app.py
@@ -2186,4 +2186,4 @@ def get_section_E(department, user_id):
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
