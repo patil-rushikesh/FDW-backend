8d613f0bc3990d748f6c3e55178702b26f6dfe7a
aansh-45 <teamaansh@gmail.com>
2025-04-07 14:38:09 +0530

    public ip added

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ app = Flask(__name__)
 # Configure CORS
 CORS(app, resources={
     r"/*": {
-        "origins": ["http://localhost:5173"],  # Your React app's URL
+        "origins": ["http://10.10.1.18:5173", "http://127.0.0.1:5173","http://localhost:5173"],  # Your React app's URLs
         "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         "allow_headers": ["Content-Type", "Authorization"],
         "supports_credentials": True
